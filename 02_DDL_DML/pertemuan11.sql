8d7143af5ed1be3c4ee35fc078cbd4a4b3fcb0f0
Dewi Atika Muthi <136959602+tikature@users.noreply.github.com>
2026-03-05 13:49:49 +0700

    Add files via upload

---
+++ 02_DDL_DML/pertemuan11.sql
@@ -0,0 +1,26 @@
+SQL> desc buah;
+ERROR:
+ORA-04043: object buah does not exist 
+
+
+SQL> desc kategori_b;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ID_KATEGORI                               NOT NULL CHAR(5)
+ KATEGORI                                           VARCHAR2(60)
+
+SQL> --nama:atika
+SQL> --nim:221110042
+SQL> desc kategori_b;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ID_KATEGORI                               NOT NULL CHAR(5)
+ KATEGORI                                           VARCHAR2(60)
+
+SQL> desc kategori_b;
+ Name                                      Null?    Type
+ ----------------------------------------- -------- ----------------------------
+ ID_KATEGORI                               NOT NULL CHAR(5)
+ KATEGORI                                           VARCHAR2(60)
+
+SQL> SPOOL OFF
